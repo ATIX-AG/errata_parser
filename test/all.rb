ca164a7b1e1675c22210bc7a109d0d31eff806f2
Markus Bucher <bucher@atix.de>
2022-07-15 23:18:11 +0200

    Rename test files

--- test/all.rb
+++ test/all.rb
@@ -3,5 +3,5 @@
 Dir.foreach(File.dirname(__FILE__)) do |filename|
   next if filename == File.basename(__FILE__)
 
-  require_relative filename if filename.end_with?('.rb')
+  require_relative filename if filename.end_with?('.rb') && filename.start_with?('test_')
 end
